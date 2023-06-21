9ddc5d5e0b3af67007e7e09925c23d429d85c782
Satoru Osawa <osawa@takram.com>
2023-06-21 20:57:25 +0900

    bluetooth serial

--- lesson 2/2_bluetooth/2_bluetooth.ino
+++ lesson 2/2_bluetooth/2_bluetooth.ino
@@ -5,27 +5,12 @@ BluetoothSerial SerialBT;
 int number = 0;
 
 void setup() {
-  M5.Lcd.begin();
-  SerialBT.begin("ESP32test");
-
-  M5.Lcd.println("Done");
+  M5.begin();
+  SerialBT.begin("M5StackSample");  // デバイス名を設定します。自分が分かる名前に変更しましょう。
 }
 
 void loop() {
-  // if (Serial.available()) {
-  //   String sendData = Serial.readStringUntil(';');
-  //   SerialBT.print(sendData);
-  // }
-
-  // if (SerialBT.available()) {
-  //   String receiveData = SerialBT.readStringUntil(';');
-  //   Serial.print(receiveData);
-  // }
-
-  SerialBT.println(number);  // シリアル通信で文字列をPCへ送信
+  SerialBT.println(number);
   number = number + 1;
-  M5.Lcd.setCursor(0, 30);
-  M5.Lcd.print("Loop");
-  M5.Lcd.println(number);
   delay(1000);
 }
