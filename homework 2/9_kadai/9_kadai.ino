51df5cdf7054d7f7ee95f11cb50edf203c25f780
Satoru Osawa <osawa@takram.com>
2023-06-26 20:54:03 +0900

    udpまで

---
+++ homework 2/9_kadai/9_kadai.ino
@@ -0,0 +1,61 @@
+#include <M5Stack.h>
+#include "Adafruit_MPR121.h"
+
+Adafruit_MPR121 mpr121 = Adafruit_MPR121();  // タッチセンサライブラリを定義
+
+int i2c_address = 0x5A;  // タッチセンサのI2Cアドレスを指定
+
+// タッチセンサから個々のValueを取得する関数を定義しています。
+// 以後、getTouchedValue関数を呼び出すこそができます。
+bool getTouchedValue(uint16_t touched_data, int pin) {
+  return (touched_data & (1 << pin)) != 0;
+}
+
+void setup() {
+  M5.begin();
+
+  M5.Lcd.setTextSize(2);
+  if (!mpr121.begin(i2c_address)) {
+    M5.Lcd.println("MPR121 is not found.");
+    while (1)
+      ;
+  }
+
+  M5.Speaker.setVolume(3);  // スピーカーのボリュームを設定 初期値は8で、範囲は 0 ~ 10
+}
+
+void loop() {
+  M5.update();  // ボタンかスピーカーを使うときは、この行を追加
+  // タッチデータを取得
+  uint16_t touched_data = mpr121.touched();
+
+  M5.Lcd.setCursor(0, 30);
+  for (int i = 0; i < 3; i++) {
+    // touched_dataから、個々のValueを取得
+    int y = i * 75 + 15;
+    for (int j = 0; j < 3; j++) {
+      int x = j * 100 + 20;
+      int pin_index = j + i * 3;
+      bool touched_value = getTouchedValue(touched_data, pin_index);
+      int freq = 100 + pin_index * 100;
+      if (touched_value) {
+        M5.Speaker.tone(freq, 40);  // 3135Hzの音を40ミリ秒再生
+        M5.Lcd.fillRect(x, y, 80, 60, WHITE);
+      } else {
+        M5.Lcd.fillRect(x, y, 80, 60, DARKGREY);
+      }
+    }
+  }
+
+  for (int i = 0; i < 9; i++) {
+    // touched_dataから、個々のValueを取得
+    bool touched_value = getTouchedValue(touched_data, i);
+    if (i != 0) {
+      Serial.print(",");
+    }
+    Serial.print(touched_value);
+  }
+  Serial.println();
+
+  // delay(10);
+}
--- lesson 2/6_udp/6_udp.ino
+++ lesson 2/6_udp/6_udp.ino
@@ -1,13 +1,13 @@
+#include <M5Stack.h>
 #include <WiFi.h>
 #include <WiFiUDP.h>
-#include <M5Stack.h>
 
-String ssid = "Buffalo-G-8500";     // WiFiのSSIDを指定
-String password = "55dbda7bfde68";  // WiFiパスワードを指定
+String ssid = "iamas-de";        // WiFiのSSIDを指定
+String password = "pass-iamas";  // WiFiパスワードを指定
 
 // Wi-Fi UDP の設定
 WiFiUDP wifi_udp;
-String pc_ip = "192.168.1.7";  // PCのIPアドレス
+String pc_ip = "192.168.2.5";  // PCのIPアドレス
 int pc_port = 12000;           // 送信先のポート
 int m5_port = 12001;           // M5Stackのポート
 
@@ -27,18 +27,27 @@ void setup() {
   }
 
   M5.Lcd.println(" connected.");
+  delay(3000);  // "connected."を3秒間表示してから画面を消します。
+  M5.Lcd.fillScreen(BLACK);
 
-  // Wi-Fi UDP へ接続開始
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.print("IP Address: ");
+  M5.Lcd.println(WiFi.localIP());  // M5StackのIPアドレスを表示
+
+  // Wi-Fi UDP 通信の開始
   wifi_udp.begin(m5_port);
 }
 
 void loop() {
   // パケット開始
   wifi_udp.beginPacket(pc_ip.c_str(), pc_port);
-  wifi_udp.println(number);  // udp通信で文字列をPCへ送信
+  wifi_udp.print(number);      // UDP通信でnumberのデータをPCへ送信
+  wifi_udp.print(",");         // UDP通信で "," をPCへ送信
+  wifi_udp.println(random());  // UDP通信で乱数をPCへ送信
   wifi_udp.endPacket();
   // パケット終了
 
   number++;
+
   delay(1000);
 }
